83daca86c4ea288d9d19586cde8f0a8e572dbb66
rex-9 <htetnaing0814@gmail.com>
2023-09-28 14:38:27 +0630

    form search

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -13,18 +13,26 @@ const Home = () => {
       <nav className="text-2xl font-bold flex-center !justify-between gap-4 py-6 px-8 font-quicksand">
         <div>Dashboard</div>
         <div className="flex-center gap-8 font-mooli">
-          <div className="relative w-[18vw]">
-            <input
-              type="search"
-              id="default-search"
-              className="relative w-full p-2 pl-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search ..."
-              required
-            />
-            <button className="absolute right-6 bottom-[20%]">
-              <Icon icon={icons.lens} />
-            </button>
-          </div>
+          <form>
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative w-[18vw]">
+              <input
+                type="search"
+                id="default-search"
+                className="relative w-full p-2 pl-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search ..."
+                required
+              />
+              <button type="submit" className="absolute right-6 bottom-[20%]">
+                <Icon icon={icons.lens} />
+              </button>
+            </div>
+          </form>
           <div className="flex-center h-12 w-[18vw] gap-4 !justify-around bg-white rounded-lg pr-4">
             <img
               className="w-20 h-20 rounded-lg"
@@ -35,6 +43,7 @@ const Home = () => {
             <Icon icon={icons.mail} />
             <button className="relative">
               <Icon icon={icons.lens} />
+              <span className="sr-only">Notifications</span>
               <div className="absolute -top-3 -right-2 inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-purple-500 border-2 border-white rounded-full">
                 5
               </div>
